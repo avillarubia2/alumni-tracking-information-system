28382e18a9eb66ad3fb733240047fac2b0b47316
vinsdgreat <vinsalegrado@gmail.com>
2020-02-20 16:59:49 +0800

    alumnis and profile UI

--- src/components/thumbnail/thumbnail.jsx
+++ src/components/thumbnail/thumbnail.jsx
@@ -9,10 +9,22 @@ const Thumbnail = ({size}) => {
         thumbSize = '60px'
     else if (size == 'xl')
         thumbSize = '90px'
+    else if (size == "contain")
+        thumbSize = "100%"
+
+    var thumbWidth = thumbSize
+    var thumbHeight = thumbSize
+    var thumbPadding = "0";
+
+    if (size == "contain") {
+        thumbHeight = "auto"
+        thumbPadding = "100% 0 0 0"
+    }
 
     const thumbStyle = {
-        width: thumbSize,
-        height: thumbSize,
+        width: thumbWidth,
+        height: thumbHeight,
+        padding: thumbPadding,
         backgroundColor: '#ddd',
         borderRadius: '50%',
         float: 'left',
