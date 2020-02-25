9a39fd638c1d60b9ca2995f4e0104b84f35682ab
aljohnv <johnnjill3@gmail.com>
2020-02-25 14:44:51 +0800

    added form validator

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,39 +2,67 @@ import React, { useState } from 'react'
 import { Row, Col, Card, Button, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { login } from '../services/auth'
+import Message from '../components/validator/message'
+import validator from '../components/validator/validator'
 
 const Login = () => {
-    const [payload, setPayload] = useState({ email: '', password: '' })
+    const [payload, setPayload] = useState({})
+    const [error, setError] = useState({})
 
     const handleLogin = async () => {
         await login(payload)
     }
 
     const handleTextChange = ({ target: { name, value } }) => {
+        validator(error, setError, name, value)
         setPayload({ ...payload, [name]: value })
     }
 
     return (
-        <section style={{ position: "fixed", width: "100%", height: "100%" }}>
-            <Col className="h-100">
-                <Row className="justify-content-center h-100">
-                    <Col md="8" lg="6" xl="4" className="center-xy">
-                        <div className="mt-n5 w-100">
-                            <h1 className="mb-4 text-center text-primary">ATISLOGO</h1>
-                            <Card className="p-5 mb-4">
-                                <Form.Group controlId="login-email">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control name="email" placeholder="Email" onChange={handleTextChange} />
-                                </Form.Group>
-                                <Form.Group className="mb-5" controlId="login-password">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control name="password" type="password" placeholder="Password" onChange={handleTextChange} />
-                                </Form.Group>
-                                <Button className="mb-3" onClick={handleLogin}>Login</Button>
-                                <p className="text-center">or</p>
-                                <Button variant="outline-dark">Register</Button>
-                            </Card>
-                            <p className="text-center"><Link to="#">lost your password?</Link></p>
+        <section style={{ position: 'fixed', width: '100%', height: '100%' }}>
+            <Col className='h-100'>
+                <Row className='justify-content-center h-100'>
+                    <Col md='8' lg='6' xl='4' className='center-xy'>
+                        <div className='mt-n5 w-100'>
+                            <h1 className='mb-4 text-center text-primary'>ATISLOGO</h1>
+                            <Form >
+                                <Card className='p-5 mb-4'>
+                                    <Form.Group controlId='login-email'>
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control
+                                            required
+                                            type='text'
+                                            name='email'
+                                            placeholder='Email'
+                                            onChange={handleTextChange}
+                                            isValid={!error.email}
+                                        />
+                                        <Message message={error.email} />
+                                    </Form.Group>
+                                    <Form.Group className='mb-5' controlId='login-password'>
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control
+                                            required
+                                            name='password'
+                                            type='password'
+                                            placeholder='Password'
+                                            onChange={handleTextChange}
+                                            isValid={!error.password}
+                                        />
+                                        <Message message={error.password} />
+                                    </Form.Group>
+                                    <Button
+                                        className='mb-3'
+                                        onClick={handleLogin}
+                                        disabled={Object.values(error).length}
+                                    >
+                                        Login
+                                    </Button>
+                                    <p className='text-center'>or</p>
+                                    <Button variant='outline-dark' >Register</Button>
+                                </Card>
+                            </Form>
+                            <p className='text-center'><Link to='#'>lost your password?</Link></p>
                         </div>
                     </Col>
                 </Row>
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -2,27 +2,31 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Card, Button, Form } from 'react-bootstrap'
 import { register } from '../services/register'
+import Message from '../components/validator/message'
+import validator from '../components/validator/validator'
 
 const Register = () => {
-    const [payload, setPayload] = useState({
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-        confirm_password: ''
-    })
+    const [payload, setPayload] = useState({})
+    const [error, setError] = useState({})
 
     const handleLogin = async () => {
+        const { password, confirm_password } = payload
+        if (password !== confirm_password)
+            setError({ ...error, 'differentPasswords': 'Please match the passwords.' })
+        else setError({ ...error, 'differentPasswords': '' })
+
         delete payload['confirm_password']
         await register(payload)
     }
 
     const handleTextChange = ({ target: { name, value } }) => {
+        validator(error, setError, name, value)
         setPayload({ ...payload, [name]: value })
     }
 
     return (
         <section style={{ position: 'fixed', width: '100%', height: '100%' }}>
+            {console.log(error)}
             <Col className='h-100'>
                 <Row className='justify-content-center h-100'>
                     <Col md='8' lg='6' xl='4' className='center-xy'>
@@ -32,25 +36,58 @@ const Register = () => {
                                 <Card.Title>Create an account</Card.Title>
                                 <Form.Group>
                                     <Form.Label>Firstname</Form.Label>
-                                    <Form.Control name='first_name' placeholder='Firstname' onChange={handleTextChange} />
+                                    <Form.Control
+                                        required
+                                        name='first_name'
+                                        placeholder='Firstname'
+                                        isValid={!error.first_name}
+                                        onChange={handleTextChange} />
+                                    <Message message={error.first_name} />
                                 </Form.Group>
                                 <Form.Group>
                                     <Form.Label>Lastname</Form.Label>
-                                    <Form.Control name='last_name' placeholder='Lastname' onChange={handleTextChange} />
+                                    <Form.Control
+                                        required
+                                        name='last_name'
+                                        placeholder='Lastname'
+                                        isValid={!error.last_name}
+                                        onChange={handleTextChange} />
+                                    <Message message={error.last_name} />
                                 </Form.Group>
                                 <Form.Group>
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control name='email' placeholder='Email' onChange={handleTextChange} />
+                                    <Form.Control
+                                        required
+                                        name='email'
+                                        placeholder='Email'
+                                        isValid={!error.email}
+                                        onChange={handleTextChange} />
+                                    <Message message={error.email} />
                                 </Form.Group>
                                 <Form.Group>
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control name='password' type='password' placeholder='Password' onChange={handleTextChange} />
+                                    <Form.Control
+                                        required
+                                        name='password'
+                                        type='password'
+                                        placeholder='Password'
+                                        isValid={!error.password}
+                                        onChange={handleTextChange} />
+                                    <Message message={error.password} />
                                 </Form.Group>
                                 <Form.Group className='mb-5' controlId='login-confirmpassword'>
                                     <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control name='confirm_password' type='password' placeholder='Confirm Password' onChange={handleTextChange} />
+                                    <Form.Control
+                                        required
+                                        name='confirm_password'
+                                        type='password'
+                                        placeholder='Confirm Password'
+                                        isValid={!error.confirm_password}
+                                        onChange={handleTextChange} />
+                                    <Message message={error.confirm_password} />
                                 </Form.Group>
-                                <Button variant='primary' onClick={handleLogin}>Register</Button>
+                                <Button variant='primary' onClick={handleLogin} disabled={Object.values(error).length}>Register</Button>
+                                <Message message={error.differentPasswords} />
                             </Card>
                             <p className='text-center'>already have account? <Link to='/login'>login here</Link></p>
                         </div>
